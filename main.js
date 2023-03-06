e06adb70f09a3c722a3f98ca246a636f027dc125
uncannysmile <sprinklingsunshower@gmail.com>
2023-03-06 08:25:02 -0800

    initial comments

---
+++ main.js
@@ -0,0 +1,40 @@
+// space dude in javascript
+
+
+
+// declaring variables/constructors
+
+
+
+// array containing all the possible words
+
+
+
+// resetting the game
+
+
+
+//random word selector
+
+
+
+//user inputting guesses
+
+
+
+//adding previously guessed letters to an array
+
+
+
+//checking if guessed letter is in the randomly chosen word
+
+
+
+//correct guess logic
+
+
+
+//incorrect guess logic
+
+
+
